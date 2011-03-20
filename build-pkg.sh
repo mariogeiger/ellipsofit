521d080c05301c25871309ccb4cba78c84b00b07
Mario Geiger <geiger.mario@gmail.com>
2011-03-20 10:16:38 +0100

    add README and add comments in build-pkg.sh

--- build-pkg.sh
+++ build-pkg.sh
@@ -10,7 +10,19 @@ gpg --list-keys
 echo -n "gpg key : "
 read gpgkey
 
+
+
+
+# -k<gpgkey>                       construire un paquet pour un envoi sponsorisé
+# -S                                ?
+# -sa                               ?
+# --lintian-opts                    lintian option used to run lintian
+# lintian -i, --info                give detailed info about tags
+
 debuild -k0x$gpgkey -S -sa --lintian-opts -i || exit 0
+
+
+
 cd ..
 
 
@@ -19,8 +31,11 @@ do
 
 	clear
 	echo "=== BEGIN $arch ==="
+	
 	sudo pbuilder --clean || exit 0
+	
 	sudo ARCH=$arch pbuilder --create --architecture $arch || exit 0
+	
 	sudo ARCH=$arch pbuilder --build --architecture $arch --buildresult . ellipsofit_${version}-1.dsc || exit 0
 
 done
